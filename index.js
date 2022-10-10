8b88a27f3c370e2de82c82e74c4356dadf22558a
ANSAB UDDIN <ansabuddin0346@gmail.com>
2022-10-10 02:04:02 -0700

    new node

--- index.js
+++ index.js
@@ -15,40 +15,40 @@ app.use(cors());
 
 // add routes
 // const router = require('./routes/routers');
-app.post('/sign-up', userMiddleware.validateRegister, (req, res, field) => {
-  const mydata = {
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password
-  }
-  pool.query(
-    `SELECT * FROM user WHERE email = ?;`, [mydata.email],
-    (err, result) => {
-      if (result && result.length) {
-        return res.status(409).send({
-          msg: 'This email is already in use!'
-        });
-      } else {
-        const salt = genSaltSync(10);
-        mydata.password = hashSync(mydata.password, salt);
-        pool.query('INSERT INTO user SET ?', mydata, (error, result) => {
+// app.post('/sign-up', userMiddleware.validateRegister, (req, res, field) => {
+//   const mydata = {
+//     username: req.body.username,
+//     email: req.body.email,
+//     password: req.body.password
+//   }
+//   pool.query(
+//     `SELECT * FROM user WHERE email = ?;`, [mydata.email],
+//     (err, result) => {
+//       if (result && result.length) {
+//         return res.status(409).send({
+//           msg: 'This email is already in use!'
+//         });
+//       } else {
+//         const salt = genSaltSync(10);
+//         mydata.password = hashSync(mydata.password, salt);
+//         pool.query('INSERT INTO user SET ?', mydata, (error, result) => {
 
-          // if (error) error;
-          // resp.send(result);
-          if (error) {
-            console.log(error);
-          }
-          return res.status(200).json({
-            success: 1,
-            message: "User Register Sucessfully",
-            data: result
-          });
-        });
+//           // if (error) error;
+//           // resp.send(result);
+//           if (error) {
+//             console.log(error);
+//           }
+//           return res.status(200).json({
+//             success: 1,
+//             message: "User Register Sucessfully",
+//             data: result
+//           });
+//         });
 
-      }
-    }
-  );
-});
+//       }
+//     }
+//   );
+// });
 app.get("/", (req, resp) => {
     pool.query("select * from user", (err, result) => {
   
